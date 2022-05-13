b27fdb1f48a06849a6cdbcaf93376ef381f9b8e0
Saritha Sujith <pillai.sujith.saritha@gmail.com>
2022-05-13 18:31:09 +0200

    changes made

--- controllers/hospital.js
+++ controllers/hospital.js
@@ -1,6 +1,6 @@
 import Hospital from '../models/hospital.js'
 
-const getHospitals = async (req, res) => {
+export const getHospitals = async (req, res) => {
   try {
     const hospitals = await Hospital.find({})
     await res.json(hospitals)
@@ -8,4 +8,36 @@ const getHospitals = async (req, res) => {
     res.status(500).send('Error getting hospitals')
   }
 }
-export default getHospitals
+
+export const postHospital = async (req, res) => {
+  try {
+    const { name, city, description } = req.body
+    const hospitals = await Hospital.create({ name, city, description })
+    await res.json(hospitals)
+  } catch (error) {
+    res.status(500).send('Error posting hospital')
+  }
+}
+
+export const getHospitalById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const hospital = await Hospital.findById(id)
+    await res.json(hospital)
+  } catch (error) {
+    res.status(500).send('Error getting hospital')
+  }
+}
+
+export const getHospitalsByKeyword = async (req, res) => {
+  try {
+    const { keyword } = req.params
+    const hospitals = await Hospital.find({
+      name: { $regex: keyword },
+    })
+
+    await res.json(hospitals)
+  } catch (error) {
+    res.status(500).send('Error getting Hospitals')
+  }
+}
