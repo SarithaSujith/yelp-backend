cc46feda0092d306b5a90382f3318243caa518fa
Saritha Sujith <pillai.sujith.saritha@gmail.com>
2022-05-13 15:50:09 +0200

    added the schema and model

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ import dotenv from 'dotenv'
 dotenv.config('./.env')
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
+import getHospitals from './routes/hospital.js'
+
+const { PORT, MNG_DB, CORS } = process.env
+const port = PORT || 5000
 
 const app = express()
 
-const { PORT, MNG_DB } = process.env
 mongoose.connect(MNG_DB, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
-//one time excecute , its connected or not
 db.once('open', () => console.log('Connected to Mongoose.'))
 
+app.use(cors({ origin: CORS }))
+app.use(express.json())
 
+app.use('/api', getHospitals)
 
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(port, () => console.log(`server started on port ${port}`))
